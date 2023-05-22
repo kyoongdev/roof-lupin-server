82438ac555880dce4697e5f12b7c3c8c7c543e4e
kyoongdev <9898junjun2@gmail.com>
2023-05-22 19:00:07 +0900

    feat(module): admin host space user

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,3 +1,7 @@
 import { GlobalModule } from './global';
+import { SpaceModule } from './space/space.module';
+import { UserModule } from './user/user.module';
+import { HostModule } from './host/host.module';
+import { AdminModule } from './admin/admin.module';
 
-export const Modules = [GlobalModule];
+export const Modules = [GlobalModule, SpaceModule, UserModule, HostModule, AdminModule];
