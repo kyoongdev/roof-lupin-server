b46d1ad1e647d77a5b4d457bc3ba7bda8162a56b
kyoongdev <9898junjun2@gmail.com>
2023-05-23 18:56:12 +0900

    feat(repository): host

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
+import { UserRepository } from 'modules/user/user.repository';
 
 @Module({
-  providers: [AdminService],
-  controllers: [AdminController]
+  providers: [AdminService, UserRepository],
+  controllers: [AdminController],
 })
 export class AdminModule {}
