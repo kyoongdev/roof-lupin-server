a8b6359bedd58cd0d4b6a69f1d3126edf2f91dd6
kyoongdev <9898junjun2@gmail.com>
2023-05-23 17:44:53 +0900

    feat(deploy): ecr iam

--- src/modules/review/review.controller.ts
+++ src/modules/review/review.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('review')
-export class ReviewController {}
+export class ReviewController {
+  @Get()
+  getReviews(): string {
+    return 'getReviews';
+  }
+}
