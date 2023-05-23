4241b0b5092986e0b4778fe1938bf26f05862ed4
kyoongdev <9898junjun2@gmail.com>
2023-05-23 11:50:56 +0900

    feat(tdd): user service before each settings

--- test/unit/user/user.service.spec.ts
+++ test/unit/user/user.service.spec.ts
@@ -1,10 +1,12 @@
 import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Prisma, User } from '@prisma/client';
 import { PrismaService } from 'database/prisma.service';
 import { UserService } from 'modules/user/user.service';
 
 describe('UserService', () => {
   let service: UserService;
+  let database: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -13,6 +15,19 @@ describe('UserService', () => {
     }).compile();
 
     service = module.get<UserService>(UserService);
+    database = module.get<PrismaService>(PrismaService);
+  });
+
+  beforeEach(async () => {
+    const users: Prisma.UserCreateInput[] = [
+      {
+        email: 'testUser@naver.com',
+        name: 'testUser1',
+        nickname: 'testUser',
+        phoneNumber: '010-4040-4040',
+      },
+    ];
+    await Promise.all(users.map((user) => database.user.create({ data: user })));
   });
 
   it('should be defined', () => {
