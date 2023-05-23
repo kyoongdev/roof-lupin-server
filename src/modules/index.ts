50c45352740cadae666847356759a73eacbe78db
kyoongdev <9898junjun2@gmail.com>
2023-05-23 11:08:14 +0900

    feat(module): report

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -3,5 +3,6 @@ import { SpaceModule } from './space/space.module';
 import { UserModule } from './user/user.module';
 import { HostModule } from './host/host.module';
 import { AdminModule } from './admin/admin.module';
+import { ReportModule } from './report/report.module';
 
-export const Modules = [GlobalModule, SpaceModule, UserModule, HostModule, AdminModule];
+export const Modules = [GlobalModule, SpaceModule, UserModule, HostModule, AdminModule, ReportModule];
