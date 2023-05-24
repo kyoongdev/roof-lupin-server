891580355cef5f3590fa38bd7cade43b64947df6
kyoongdev <9898junjun2@gmail.com>
2023-05-24 18:03:24 +0900

    chore: healthy check response status

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import type { Response as ResponseType } from 'express';
+import { Controller, Get, Response } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello() {
-    return { healthy: 'healthy' };
+  @Get('/health')
+  healthCheck(@Response() response: ResponseType) {
+    response.status(200).json({ status: 'HEALTHY' });
   }
 
   @Get('/test')
