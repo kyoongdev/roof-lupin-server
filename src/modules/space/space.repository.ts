cd6faeee35741e480ec1b018ba20d5badcc2f912
kyoongdev <9898junjun2@gmail.com>
2023-06-01 17:54:29 +0900

    feat(space): like & interest repository

--- src/modules/space/space.repository.ts
+++ src/modules/space/space.repository.ts
@@ -25,4 +25,61 @@ export class SpaceRepository {
 
     return space;
   }
+
+  async createLike(userId: string, spaceId: string) {
+    await this.database.spaceLike.create({
+      data: {
+        userId,
+        spaceId,
+      },
+    });
+  }
+
+  async deleteLike(userId: string, spaceId: string) {
+    await this.database.spaceLike.deleteMany({
+      where: {
+        userId,
+        spaceId,
+      },
+    });
+  }
+
+  async checkIsLiked(userId: string, spaceId: string) {
+    const like = await this.database.spaceLike.findFirst({
+      where: {
+        userId,
+        spaceId,
+      },
+    });
+
+    return like ? true : false;
+  }
+  async createInterest(userId: string, spaceId: string) {
+    await this.database.spaceInterest.create({
+      data: {
+        userId,
+        spaceId,
+      },
+    });
+  }
+
+  async deleteInterest(userId: string, spaceId: string) {
+    await this.database.spaceInterest.deleteMany({
+      where: {
+        userId,
+        spaceId,
+      },
+    });
+  }
+
+  async checkIsInterested(userId: string, spaceId: string) {
+    const interest = await this.database.spaceInterest.findFirst({
+      where: {
+        userId,
+        spaceId,
+      },
+    });
+
+    return interest ? true : false;
+  }
 }
