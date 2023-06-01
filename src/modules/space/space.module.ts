956ebb5312c3bc0cf91b2571cf813d7a1d3df4f7
kyoongdev <9898junjun2@gmail.com>
2023-06-01 18:00:44 +0900

    feat(space): like & interest service

--- src/modules/space/space.module.ts
+++ src/modules/space/space.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 
 import { ClientRevalidateEvent } from '@/event/client';
 
+import { UserRepository } from '../user/user.repository';
+
 import { SpaceController } from './space.controller';
 import { SpaceRepository } from './space.repository';
 import { SpaceService } from './space.service';
 
 @Module({
-  providers: [SpaceService, SpaceRepository, ClientRevalidateEvent],
+  providers: [SpaceService, SpaceRepository, ClientRevalidateEvent, UserRepository],
   controllers: [SpaceController],
 })
 export class SpaceModule {}
--- src/modules/space/space.service.ts
+++ src/modules/space/space.service.ts
@@ -1,4 +1,64 @@
 import { Injectable } from '@nestjs/common';
 
+import { UserRepository } from '../user/user.repository';
+
+import { ALREADY_INTERESTED, ALREADY_LIKED, NOT_INTERESTED, NOT_LIKED, SPACE_ERROR_CODE } from './exception/errorCode';
+import { SpaceException } from './exception/space.exception';
+import { SpaceRepository } from './space.repository';
+
 @Injectable()
-export class SpaceService {}
+export class SpaceService {
+  constructor(private readonly userRepository: UserRepository, private readonly spaceRepository: SpaceRepository) {}
+
+  async findSpace(id: string) {
+    return await this.spaceRepository.findSpace(id);
+  }
+
+  async createLike(userId: string, spaceId: string) {
+    await this.findSpace(spaceId);
+
+    const isLiked = await this.spaceRepository.checkIsInterested(userId, spaceId);
+
+    if (isLiked) {
+      throw new SpaceException(SPACE_ERROR_CODE.CONFLICT(ALREADY_LIKED));
+    }
+
+    await this.spaceRepository.createLike(userId, spaceId);
+  }
+
+  async deleteLike(userId: string, spaceId: string) {
+    await this.findSpace(spaceId);
+
+    const isLiked = await this.spaceRepository.checkIsInterested(userId, spaceId);
+
+    if (!isLiked) {
+      throw new SpaceException(SPACE_ERROR_CODE.CONFLICT(NOT_LIKED));
+    }
+
+    await this.spaceRepository.deleteLike(userId, spaceId);
+  }
+
+  async createInterest(userId: string, spaceId: string) {
+    await this.findSpace(spaceId);
+
+    const isInterested = await this.spaceRepository.checkIsInterested(userId, spaceId);
+
+    if (isInterested) {
+      throw new SpaceException(SPACE_ERROR_CODE.CONFLICT(ALREADY_INTERESTED));
+    }
+
+    await this.spaceRepository.createInterest(userId, spaceId);
+  }
+
+  async deleteInterest(userId: string, spaceId: string) {
+    await this.findSpace(spaceId);
+
+    const isInterested = await this.spaceRepository.checkIsInterested(userId, spaceId);
+
+    if (!isInterested) {
+      throw new SpaceException(SPACE_ERROR_CODE.CONFLICT(NOT_INTERESTED));
+    }
+
+    await this.spaceRepository.deleteInterest(userId, spaceId);
+  }
+}
