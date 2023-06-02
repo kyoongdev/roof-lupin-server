f08ea8f1e507e7869c956616e3ee5b8bc1402d55
kyoongdev <9898junjun2@gmail.com>
2023-06-02 18:40:53 +0900

    feat(host): report service

--- src/modules/host/report/report.service.ts
+++ src/modules/host/report/report.service.ts
@@ -1,8 +1,35 @@
 import { Injectable } from '@nestjs/common';
 
+import { Prisma } from '@prisma/client';
+import { PaginationDTO, PagingDTO } from 'wemacu-nestjs';
+
+import { ReportDTO } from '@/modules/report/dto';
 import { ReportRepository } from '@/modules/report/report.repository';
 
 @Injectable()
 export class HostReportService {
   constructor(private readonly reportRepository: ReportRepository) {}
+
+  async findReport(id: string) {
+    return await this.reportRepository.findReport(id);
+  }
+
+  async findPagingReports(paging: PagingDTO, args = {} as Prisma.SpaceReportFindManyArgs) {
+    const { skip, take } = paging.getSkipTake();
+    const count = await this.reportRepository.countReports({
+      where: {
+        ...args.where,
+        deletedAt: null,
+      },
+    });
+    const reports = await this.reportRepository.findReports({
+      where: {
+        ...args.where,
+        deletedAt: null,
+      },
+      skip,
+      take,
+    });
+    return new PaginationDTO<ReportDTO>(reports, { paging, count });
+  }
 }
