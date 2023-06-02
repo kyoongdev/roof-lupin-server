c617188867958153d03e3d89447a04e97144befd
kyoongdev <9898junjun2@gmail.com>
2023-06-02 16:03:22 +0900

    feat(host): service

--- src/modules/host/host.service.ts
+++ src/modules/host/host.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
-import { UpdateHostDTO } from './dto';
+import { CreateHostAccountDTO, UpdateHostAccountDTO, UpdateHostDTO } from './dto';
 import { HostRepository } from './host.repository';
 
 @Injectable()
@@ -20,6 +20,31 @@ export class HostService {
   }
 
   async updateHost(id: string, data: UpdateHostDTO) {
+    await this.findHost(id);
     await this.hostRepository.updateHost(id, data);
+  }
+
+  async deleteHost(id: string) {
+    await this.findHost(id);
+    await this.hostRepository.deleteHost(id);
+  }
+
+  async hardDeleteHost(id: string) {
+    await this.findHost(id);
+    await this.hostRepository.hardDeleteHost(id);
+  }
+
+  async createHostAccount(hostId: string, data: CreateHostAccountDTO) {
+    return await this.hostRepository.createHostAccount(hostId, data);
+  }
+
+  async updateHostAccount(id: string, data: UpdateHostAccountDTO) {
+    await this.findHostAccount(id);
+    await this.hostRepository.updateHostAccount(id, data);
+  }
+
+  async deleteHostAccount(id: string) {
+    await this.findHostAccount(id);
+    await this.hostRepository.deleteHostAccount(id);
   }
 }
