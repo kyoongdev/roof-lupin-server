71de08c0b874283c8adfcff9114c98ba2d61af10
kyoongdev <9898junjun2@gmail.com>
2023-06-05 19:01:04 +0900

    fix(host): module injection

--- src/modules/host/space/index.ts
+++ src/modules/host/space/index.ts
@@ -1,8 +1,8 @@
-import { SpaceService } from '@prisma/client';
-
 import { ApiController } from '@/utils';
+
+import { HostSpaceService } from './space.service';
 
 @ApiController('hosts/spaces', '[호스트] 공간 관리')
 export class HostSpaceController {
-  constructor(private readonly spaceService: SpaceService) {}
+  constructor(private readonly spaceService: HostSpaceService) {}
 }
