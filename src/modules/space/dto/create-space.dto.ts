e2cd9c641f0ee8f684a123423ca7c32741e33d49
kyoongdev <9898junjun2@gmail.com>
2023-06-05 18:28:01 +0900

    feat(dto): create space dto

--- src/modules/space/dto/create-space.dto.ts
+++ src/modules/space/dto/create-space.dto.ts
@@ -1,13 +1,16 @@
-import { LocationDTOProps } from '@/modules/location/dto';
+import { Property } from 'wemacu-nestjs';
 
-import { CreateSpaceCategoryDTOProps } from './category';
-import { CreateCautionDTO, CreateCautionDTOProps } from './caution';
-import { CreateFacilityDTOProps } from './facility';
-import { CreateHashtagDTO, CreateHashtagDTOProps } from './hashtag';
-import { CreateRefundPolicyDTOProps } from './refund';
-import { CreateRentalTypeDTOProps } from './rentalType';
-import { CreateServiceDTOProps } from './service';
-import { CreateTransportationDTOProps } from './transportaion';
+import { LocationDTO, type LocationDTOProps } from '@/modules/location/dto';
+
+import { CreateSpaceCategoryDTO, type CreateSpaceCategoryDTOProps } from './category';
+import { CreateCautionDTO, type CreateCautionDTOProps } from './caution';
+import { CreateFacilityDTO, type CreateFacilityDTOProps } from './facility';
+import { CreateHashtagDTO, type CreateHashtagDTOProps } from './hashtag';
+import { CreateRefundPolicyDTO, type CreateRefundPolicyDTOProps } from './refund';
+import { CreateRentalTypeDTO, type CreateRentalTypeDTOProps } from './rentalType';
+import { CreateServiceDTO, type CreateServiceDTOProps } from './service';
+import { CreateSizeDTO, type CreateSizeDTOProps } from './size';
+import { CreateTransportationDTO, type CreateTransportationDTOProps } from './transportaion';
 
 export interface CreateSpaceDTOProps {
   title: string;
@@ -29,6 +32,94 @@ export interface CreateSpaceDTOProps {
   categories: CreateSpaceCategoryDTOProps[];
   hashtags: CreateHashtagDTOProps[];
   publicTransportations: CreateTransportationDTOProps[];
+  sizes: CreateSizeDTOProps[];
 }
 
-export class CreateSpaceDTO {}
+export class CreateSpaceDTO {
+  @Property({ apiProperty: { type: 'string', description: '공간 제목' } })
+  title: string;
+
+  @Property({ apiProperty: { type: 'string', description: '공간 설명' } })
+  description: string;
+
+  @Property({ apiProperty: { type: 'number', nullable: true, description: '공간 유형' } })
+  spaceType?: number;
+
+  @Property({ apiProperty: { type: 'number', nullable: true, description: '건물 유형' } })
+  buildingType?: number;
+
+  @Property({ apiProperty: { type: 'string', description: '썸네일' } })
+  thumbnail: string;
+
+  @Property({ apiProperty: { type: 'number', description: '최소 인원' } })
+  minUser: number;
+
+  @Property({ apiProperty: { type: 'number', description: '최대 인원' } })
+  maxUser: number;
+
+  @Property({ apiProperty: { type: 'number', description: '초과 인원 당 추가 금액' } })
+  overflowUserCost: number;
+
+  @Property({ apiProperty: { type: 'number', description: '초과 인원' } })
+  overflowUserCount: number;
+
+  @Property({ apiProperty: { type: 'string', isArray: true, description: '이미지' } })
+  images: string[];
+
+  @Property({ apiProperty: { type: 'string', isArray: true, description: '환불 정책' } })
+  refundPolicies: CreateRefundPolicyDTO[];
+
+  @Property({ apiProperty: { type: 'string', isArray: true, description: '주의 사항' } })
+  cautions: CreateCautionDTO[];
+
+  @Property({ apiProperty: { type: 'string', isArray: true, description: '대여 유형' } })
+  rentalTypes: CreateRentalTypeDTO[];
+
+  @Property({ apiProperty: { type: 'string', description: '위치' } })
+  location: LocationDTO;
+
+  @Property({ apiProperty: { type: 'string', isArray: true, description: '시설' } })
+  facilities: CreateFacilityDTO[];
+
+  @Property({ apiProperty: { type: 'string', isArray: true, description: '서비스' } })
+  services: CreateServiceDTO[];
+
+  @Property({ apiProperty: { type: 'string', isArray: true, description: '카테고리' } })
+  categories: CreateSpaceCategoryDTO[];
+
+  @Property({ apiProperty: { type: 'string', isArray: true, description: '해시태그' } })
+  hashtags: CreateHashtagDTO[];
+
+  @Property({ apiProperty: { type: 'string', isArray: true, description: '대중교통' } })
+  publicTransportations: CreateTransportationDTO[];
+
+  @Property({ apiProperty: { type: 'string', isArray: true, description: '면적' } })
+  sizes: CreateSizeDTO[];
+
+  constructor(props?: CreateSpaceDTOProps) {
+    if (props) {
+      this.title = props.title;
+      this.description = props.description;
+      this.spaceType = props.spaceType;
+      this.buildingType = props.buildingType;
+      this.thumbnail = props.thumbnail;
+      this.minUser = props.minUser;
+      this.maxUser = props.maxUser;
+      this.overflowUserCost = props.overflowUserCost;
+      this.overflowUserCount = props.overflowUserCount;
+      this.images = props.images;
+      this.refundPolicies = props.refundPolicies;
+      this.cautions = props.cautions;
+      this.rentalTypes = props.rentalTypes.map((rentalType) => new CreateRentalTypeDTO(rentalType));
+      this.location = new LocationDTO(props.location);
+      this.facilities = props.facilities.map((facility) => new CreateFacilityDTO(facility));
+      this.services = props.services.map((service) => new CreateServiceDTO(service));
+      this.categories = props.categories.map((category) => new CreateSpaceCategoryDTO(category));
+      this.hashtags = props.hashtags.map((hashtag) => new CreateHashtagDTO(hashtag));
+      this.publicTransportations = props.publicTransportations.map(
+        (transportation) => new CreateTransportationDTO(transportation)
+      );
+      this.sizes = props.sizes.map((size) => new CreateSizeDTO(size));
+    }
+  }
+}
