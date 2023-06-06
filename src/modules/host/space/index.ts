592bc8b021ed6daa0a536ae99a8d7bd4a00aa91e
kyoongdev <9898junjun2@gmail.com>
2023-06-06 15:58:15 +0900

    feat(host): host space apis

--- src/modules/host/space/index.ts
+++ src/modules/host/space/index.ts
@@ -1,11 +1,12 @@
-import { Body, Get, Param, Post, UseInterceptors } from '@nestjs/common';
+import { Body, Delete, Get, Param, Patch, Post, UseInterceptors } from '@nestjs/common';
 
 import { Auth, RequestApi, ResponseApi } from 'wemacu-nestjs';
 
-import { ResponseWithIdDTO } from '@/common';
+import { EmptyResponseDTO, ResponseWithIdDTO } from '@/common';
 import { RequestHost } from '@/interface/role.interface';
 import { SpaceDetailDTO, SpaceDTO } from '@/modules/space/dto';
 import { CreateSpaceDTO } from '@/modules/space/dto/create-space.dto';
+import { UpdateSpaceDTO } from '@/modules/space/dto/update-space.dto';
 import { ApiController, ReqUser, ResponseWithIdInterceptor } from '@/utils';
 import { JwtAuthGuard } from '@/utils/guards';
 import { RoleGuard } from '@/utils/guards/role.guard';
@@ -62,5 +63,79 @@ export class HostSpaceController {
   })
   async createSpace(@ReqUser() user: RequestHost, @Body() body: CreateSpaceDTO) {
     return await this.spaceService.createSpace(user.id, body);
+  }
+
+  @Patch(':spaceId')
+  @RequestApi({
+    summary: {
+      description: '공간 수정',
+      summary: '공간 수정 - 호스트만 사용가능합니다.',
+    },
+    body: {
+      type: UpdateSpaceDTO,
+    },
+    params: {
+      name: 'spaceId',
+      description: '공간 아이디',
+      required: true,
+      type: 'string',
+    },
+  })
+  @ResponseApi(
+    {
+      type: EmptyResponseDTO,
+    },
+    204
+  )
+  async updateSpace(@ReqUser() user: RequestHost, @Param('spaceId') id: string, @Body() body: UpdateSpaceDTO) {
+    await this.spaceService.updateSpace(id, user.id, body);
+  }
+
+  @Delete(':spaceId')
+  @RequestApi({
+    summary: {
+      description: '공간 삭제',
+      summary: '공간 삭제 - 호스트만 사용가능합니다.',
+    },
+
+    params: {
+      name: 'spaceId',
+      description: '공간 아이디',
+      required: true,
+      type: 'string',
+    },
+  })
+  @ResponseApi(
+    {
+      type: EmptyResponseDTO,
+    },
+    204
+  )
+  async deleteSpace(@ReqUser() user: RequestHost, @Param('spaceId') id: string) {
+    await this.spaceService.deleteSpace(id, user.id);
+  }
+
+  @Delete(':spaceId/hard')
+  @RequestApi({
+    summary: {
+      description: '공간 삭제 [하드]',
+      summary: '공간 삭제 - 호스트만 사용가능합니다.[하드]',
+    },
+
+    params: {
+      name: 'spaceId',
+      description: '공간 아이디',
+      required: true,
+      type: 'string',
+    },
+  })
+  @ResponseApi(
+    {
+      type: EmptyResponseDTO,
+    },
+    204
+  )
+  async hardDeleteSpace(@ReqUser() user: RequestHost, @Param('spaceId') id: string) {
+    await this.spaceService.hardDeleteSpace(id, user.id);
   }
 }
