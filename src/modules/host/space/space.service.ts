13a26f5eeb2576bc0506d0a9eaa611066c9b1ee6
kyoongdev <9898junjun2@gmail.com>
2023-06-06 15:53:19 +0900

    feat(host): space service

--- src/modules/host/space/space.service.ts
+++ src/modules/host/space/space.service.ts
@@ -3,9 +3,10 @@ import { Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 
 import { CreateSpaceDTO } from '@/modules/space/dto/create-space.dto';
+import { UpdateSpaceDTO } from '@/modules/space/dto/update-space.dto';
 import { SpaceRepository } from '@/modules/space/space.repository';
 
-import { HOST_ERROR_CODE, HOST_SPACE_FIND_FORBIDDEN } from '../exception/errorCode';
+import { HOST_ERROR_CODE, HOST_SPACE_FIND_FORBIDDEN, HOST_SPACE_MUTATION_FORBIDDEN } from '../exception/errorCode';
 import { HostException } from '../exception/host.exception';
 
 @Injectable()
@@ -33,5 +34,35 @@ export class HostSpaceService {
 
   async createSpace(hostId: string, data: CreateSpaceDTO) {
     return await this.spaceRepository.createSpace(hostId, data);
+  }
+
+  async updateSpace(id: string, hostId: string, data: UpdateSpaceDTO) {
+    const space = await this.spaceRepository.findSpace(id);
+
+    if (space.host.id !== hostId) {
+      throw new HostException(HOST_ERROR_CODE.FORBIDDEN(HOST_SPACE_MUTATION_FORBIDDEN));
+    }
+
+    await this.spaceRepository.updateSpace(id, data);
+  }
+
+  async deleteSpace(id: string, hostId: string) {
+    const space = await this.spaceRepository.findSpace(id);
+
+    if (space.host.id !== hostId) {
+      throw new HostException(HOST_ERROR_CODE.FORBIDDEN(HOST_SPACE_MUTATION_FORBIDDEN));
+    }
+
+    await this.spaceRepository.deleteSpace(id);
+  }
+
+  async hardDeleteSpace(id: string, hostId: string) {
+    const space = await this.spaceRepository.findSpace(id);
+
+    if (space.host.id !== hostId) {
+      throw new HostException(HOST_ERROR_CODE.FORBIDDEN(HOST_SPACE_MUTATION_FORBIDDEN));
+    }
+
+    await this.spaceRepository.hardDeleteSpace(id);
   }
 }
