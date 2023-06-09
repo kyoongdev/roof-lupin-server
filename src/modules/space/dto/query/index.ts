5cdc259a1d3420aff6f7c3a28b080843f27bb1db
kyoongdev <9898junjun2@gmail.com>
2023-06-09 17:32:35 +0900

    feat(query): possible rental type query

--- src/modules/space/dto/query/index.ts
+++ src/modules/space/dto/query/index.ts
@@ -1,1 +1,1 @@
-export {};
+export * from './possible-rental-type.query';
---
+++ src/modules/space/dto/query/possible-rental-type.query.ts
@@ -0,0 +1,18 @@
+import { Property } from 'wemacu-nestjs';
+
+export interface PossibleRentalTypeQueryProps {
+  year: string;
+  month: string;
+  day: string;
+}
+
+export class PossibleRentalTypeQuery {
+  @Property({ apiProperty: { type: 'string', description: '년도' } })
+  year: string;
+
+  @Property({ apiProperty: { type: 'string', description: '월' } })
+  month: string;
+
+  @Property({ apiProperty: { type: 'string', description: '일' } })
+  day: string;
+}
