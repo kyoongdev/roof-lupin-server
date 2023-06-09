fd0136739d486ed7a8006403ad398535dfa448b7
kyoongdev <9898junjun2@gmail.com>
2023-06-09 15:13:52 +0900

    feat(alarm): admin alarm apis

--- src/modules/admin/alarm/alarm.controller.ts
+++ src/modules/admin/alarm/alarm.controller.ts
@@ -1,9 +1,10 @@
-import { Get, Param } from '@nestjs/common';
+import { Body, Delete, Get, Param, Patch, Post, UseInterceptors } from '@nestjs/common';
 
 import { Auth, Paging, PagingDTO, RequestApi, ResponseApi } from 'wemacu-nestjs';
 
-import { AlarmDTO } from '@/modules/alarm/dto';
-import { ApiController } from '@/utils';
+import { EmptyResponseDTO, ResponseWithIdDTO } from '@/common';
+import { AlarmDTO, CreateAlarmDTO, UpdateAlarmDTO } from '@/modules/alarm/dto';
+import { ApiController, ResponseWithIdInterceptor } from '@/utils';
 import { JwtAuthGuard } from '@/utils/guards';
 import { RoleGuard } from '@/utils/guards/role.guard';
 
@@ -50,5 +51,76 @@ export class AdminAlarmController {
   })
   async getAlarm(@Param('alarmId') id: string) {
     return await this.alarmService.findAlarm(id);
+  }
+
+  @Post()
+  @UseInterceptors(ResponseWithIdInterceptor)
+  @RequestApi({
+    summary: {
+      description: '알람 생성하기',
+      summary: '알람 생성하기 - 관리자만 사용 가능합니다.',
+    },
+    body: {
+      type: CreateAlarmDTO,
+    },
+  })
+  @ResponseApi(
+    {
+      type: ResponseWithIdDTO,
+    },
+    201
+  )
+  async createAlarm(@Body() createAlarmDTO: CreateAlarmDTO) {
+    return await this.alarmService.createAlarm(createAlarmDTO);
+  }
+
+  @Patch(':alarmId')
+  @RequestApi({
+    summary: {
+      description: '알람 수정하기',
+      summary: '알람 수정하기 - 관리자만 사용 가능합니다.',
+    },
+    body: {
+      type: UpdateAlarmDTO,
+    },
+    params: {
+      name: 'alarmId',
+      type: 'string',
+      required: true,
+      description: '알람 ID',
+    },
+  })
+  @ResponseApi(
+    {
+      type: EmptyResponseDTO,
+    },
+    204
+  )
+  async updateAlarm(@Param('alarmId') id: string, @Body() body: UpdateAlarmDTO) {
+    await this.alarmService.updateAlarm(id, body);
+  }
+
+  @Delete(':alarmId')
+  @RequestApi({
+    summary: {
+      description: '알람 삭제하기',
+      summary: '알람 삭제하기 - 관리자만 사용 가능합니다.',
+    },
+
+    params: {
+      name: 'alarmId',
+      type: 'string',
+      required: true,
+      description: '알람 ID',
+    },
+  })
+  @ResponseApi(
+    {
+      type: EmptyResponseDTO,
+    },
+    204
+  )
+  async deleteAlarm(@Param('alarmId') id: string) {
+    await this.alarmService.deleteAlarm(id);
   }
 }
