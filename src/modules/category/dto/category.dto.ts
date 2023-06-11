098a39a1e11e19e14d3242b692c93a5a3f1cb76b
kyoongdev <9898junjun2@gmail.com>
2023-06-11 16:16:22 +0900

    feat(category): dto

--- src/modules/category/dto/category.dto.ts
+++ src/modules/category/dto/category.dto.ts
@@ -4,6 +4,8 @@ export interface CategoryDTOProps {
   id: string;
   name: string;
   isHome: boolean;
+  iconPath?: string;
+  isRecommended: boolean;
 }
 
 export class CategoryDTO {
@@ -13,12 +15,22 @@ export class CategoryDTO {
   @Property({ apiProperty: { type: 'string', description: '카테고리 이름' } })
   name: string;
 
+  @Property({
+    apiProperty: { type: 'string', nullable: true, description: '아이콘 경로' },
+  })
+  iconPath?: string;
+
   @Property({ apiProperty: { type: 'boolean', description: '홈 카테고리 여부' } })
   isHome: boolean;
+
+  @Property({ apiProperty: { type: 'boolean', description: '추천 여부' } })
+  isRecommended: boolean;
 
   constructor(props: CategoryDTOProps) {
     this.id = props.id;
     this.name = props.name;
+    this.iconPath = props.iconPath;
     this.isHome = props.isHome;
+    this.isRecommended = props.isRecommended;
   }
 }
--- src/modules/category/dto/update-category.dto.ts
+++ src/modules/category/dto/update-category.dto.ts
@@ -2,19 +2,31 @@ import { Property } from 'wemacu-nestjs';
 
 export interface UpdateCategoryDTOProps {
   name?: string;
+  iconPath?: string;
   isHome?: boolean;
+  isRecommended?: boolean;
 }
 export class UpdateCategoryDTO {
   @Property({ apiProperty: { type: 'string', nullable: true, description: '카테고리 이름' } })
   name: string;
 
+  @Property({
+    apiProperty: { type: 'string', nullable: true, description: '아이콘 경로, 홈 카테고리라면 아이콘 필수' },
+  })
+  iconPath?: string;
+
   @Property({ apiProperty: { type: 'boolean', nullable: true, description: '홈 카테고리 여부' } })
   isHome?: boolean;
+
+  @Property({ apiProperty: { type: 'boolean', nullable: true, description: '추천 여부' } })
+  isRecommended?: boolean;
 
   constructor(props?: UpdateCategoryDTOProps) {
     if (props) {
       this.name = props.name;
+      this.iconPath = props.iconPath;
       this.isHome = props.isHome;
+      this.isRecommended = props.isRecommended;
     }
   }
 }
