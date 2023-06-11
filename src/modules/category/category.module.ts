7fd570e579944c895c23df2af14d9c8766267025
kyoongdev <9898junjun2@gmail.com>
2023-06-11 16:37:14 +0900

    feat(category): apply category apis

--- src/modules/category/category.module.ts
+++ src/modules/category/category.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 
+import { CategoryController } from './category.controller';
 import { CategoryRepository } from './category.repository';
 import { CategoryService } from './category.service';
 
 @Module({
   providers: [CategoryRepository, CategoryService],
+  controllers: [CategoryController],
 })
 export class CategoryModule {}
