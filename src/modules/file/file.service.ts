f7676147366f326976885b478e8a9c6b1c0f6a70
kyoongdev <9898junjun2@gmail.com>
2023-06-18 17:04:05 +0900

    chore(lib): aws optimization

--- src/modules/file/file.service.ts
+++ src/modules/file/file.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
-import * as AWS from 'aws-sdk';
+import AWS from '@aws-sdk/client-s3';
 
 import { ImageDTO } from './dto';
 
@@ -10,23 +10,20 @@ export class FileService {
   constructor(private readonly configService: ConfigService) {}
 
   async uploadFile(file: Express.Multer.File) {
-    AWS.config.update({
-      region: this.configService.get('AWS_REGION'),
-      credentials: {
-        accessKeyId: this.configService.get('AWS_S3_ACCESS_KEY'),
-        secretAccessKey: this.configService.get('AWS_S3_PRIVATE_KEY'),
-      },
-    });
     try {
       const key = `${Date.now() + file.originalname}`;
 
-      await new AWS.S3()
-        .putObject({
-          Key: key,
-          Body: file.buffer,
-          Bucket: this.configService.get('AWS_S3_BUCKET_NAME'),
-        })
-        .promise();
+      await new AWS.S3({
+        region: this.configService.get('AWS_REGION'),
+        credentials: {
+          accessKeyId: this.configService.get('AWS_S3_ACCESS_KEY'),
+          secretAccessKey: this.configService.get('AWS_S3_PRIVATE_KEY'),
+        },
+      }).putObject({
+        Key: key,
+        Body: file.buffer,
+        Bucket: this.configService.get('AWS_S3_BUCKET_NAME'),
+      });
 
       const url = `${this.configService.get('AWS_S3_BUCKET_URL')}${key}`;
 
