f26ad54d42080ddf9fa523482cc9a619f557a957
kyoongdev <9898junjun2@gmail.com>
2023-06-20 15:05:28 +0900

    feat(user-coupon): user coupon service

--- src/modules/coupon/coupon.module.ts
+++ src/modules/coupon/coupon.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { CouponController } from './coupon.controller';
+import { CouponRepository } from './coupon.repository';
 import { CouponService } from './coupon.service';
 
 @Module({
   controllers: [CouponController],
-  providers: [CouponService],
+  providers: [CouponService, CouponRepository],
 })
 export class CouponModule {}
--- src/modules/coupon/coupon.service.ts
+++ src/modules/coupon/coupon.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
 
+import { PaginationDTO, PagingDTO } from 'wemacu-nestjs';
+
+import { CouponRepository } from './coupon.repository';
+import { UserCouponDTO } from './dto';
+
 @Injectable()
-export class CouponService {}
+export class CouponService {
+  constructor(private readonly couponRepository: CouponRepository) {}
+
+  async findUserCoupon(id: string) {
+    return await this.couponRepository.findUserCoupon(id);
+  }
+
+  async findPagingUserCoupons(paging: PagingDTO, userId: string) {
+    const { skip, take } = paging.getSkipTake();
+
+    const count = await this.couponRepository.countUserCoupons({
+      where: {
+        userId,
+      },
+    });
+    const coupons = await this.couponRepository.findUserCoupons({
+      where: {
+        userId,
+      },
+      skip,
+      take,
+    });
+    return new PaginationDTO<UserCouponDTO>(coupons, { count, paging });
+  }
+}
