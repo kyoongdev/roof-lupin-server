24d118c5fc51c42892df7fc784c17f292441fdf1
kyoongdev <9898junjun2@gmail.com>
2023-06-21 18:40:21 +0900

    feat(coupon): coupon categoires

---
+++ prisma/migrations/20230621094011_coupon_category/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `dueDate` on the `UserCoupon` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+
+*/
+-- AlterTable
+ALTER TABLE `UserCoupon` MODIFY `dueDate` DATETIME NOT NULL;
+
+-- CreateTable
+CREATE TABLE `CouponCategory` (
+    `couponId` VARCHAR(191) NOT NULL,
+    `categoryId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`couponId`, `categoryId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `CouponCategory` ADD CONSTRAINT `CouponCategory_couponId_fkey` FOREIGN KEY (`couponId`) REFERENCES `Coupon`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `CouponCategory` ADD CONSTRAINT `CouponCategory_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `Category`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
