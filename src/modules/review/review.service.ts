3308aa3dc1ea2609bdc016bbf63550786349419d
kyoongdev <9898junjun2@gmail.com>
2023-06-22 10:34:34 +0900

    feat(review): create review exception

--- src/modules/review/review.service.ts
+++ src/modules/review/review.service.ts
@@ -15,6 +15,7 @@ import {
   REVIEW_MUTATION_FORBIDDEN,
   REVIEW_REPORT_ALREADY_EXISTS,
   REVIEW_REPORT_MUTATION_FORBIDDEN,
+  REVIEW_SPACE_BAD_REQUEST,
   SCORE_BAD_REQUEST,
 } from './exception/errorCode';
 import { ReviewException } from './exception/review.exception';
@@ -81,6 +82,10 @@ export class ReviewService {
   async createReview(props: CreateReviewDTO, userId: string) {
     const reservation = await this.reservationRepository.findReservation(props.reservationId);
     const { score, spaceId } = props;
+
+    if (reservation.space.id !== props.spaceId) {
+      throw new ReviewException(REVIEW_ERROR_CODE.BAD_REQUEST(REVIEW_SPACE_BAD_REQUEST));
+    }
 
     if (score < 1 || score > 5) {
       throw new ReviewException(REVIEW_ERROR_CODE.BAD_REQUEST(SCORE_BAD_REQUEST));
