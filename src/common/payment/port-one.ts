bdcb4daac30471f3918d3092ec82549eae7ce97c
kyoongdev <9898junjun2@gmail.com>
2023-06-22 19:01:18 +0900

    feat(payment): [wip] portone provider

--- src/common/payment/port-one.ts
+++ src/common/payment/port-one.ts
@@ -2,9 +2,22 @@ import { Injectable } from '@nestjs/common';
 
 import axios from 'axios';
 
+import { PortOneGetPayment } from '@/interface/payment/port-one.interface';
+
 @Injectable()
 export class PortOneProvider {
   private apiClient = axios.create({
     baseURL: 'https://api.iamport.kr/payments',
   });
+
+  async getPayment(props: PortOneGetPayment) {
+    const response = await this.apiClient.get(`/${props.imp_uid}`);
+  }
+
+  getHeader(token: string) {
+    return {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    };
+  }
 }
