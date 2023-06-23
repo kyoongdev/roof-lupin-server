24d0bec639110b60bbd31c64fa9f29faf605cc34
kyoongdev <9898junjun2@gmail.com>
2023-06-23 16:05:08 +0900

    feat(image): delete image

--- src/modules/file/file.service.ts
+++ src/modules/file/file.service.ts
@@ -12,7 +12,7 @@ export class FileService {
   async uploadFile(file: Express.Multer.File) {
     try {
       const key = `${Date.now() + file.originalname}`;
-      console.log(AWS);
+
       await new AWS.S3({
         region: this.configService.get('AWS_REGION'),
         credentials: {
@@ -26,14 +26,14 @@ export class FileService {
       });
 
       const url = `${this.configService.get('AWS_CLOUD_FRONT_URL')}/${key}`;
-      console.log(url.split(this.configService.get('AWS_CLOUD_FRONT_URL')));
+
       return new ImageDTO({ url });
     } catch (error) {
       console.log(error);
     }
   }
 
-  async deleteFile() {
+  async deleteFile(url: string) {
     await new AWS.S3({
       region: this.configService.get('AWS_REGION'),
       credentials: {
@@ -41,7 +41,7 @@ export class FileService {
         secretAccessKey: this.configService.get('AWS_S3_PRIVATE_KEY'),
       },
     }).deleteObject({
-      Key: 'key',
+      Key: ImageDTO.parseS3ImageKey(url),
       Bucket: this.configService.get('AWS_S3_BUCKET_NAME'),
     });
   }
