45a309332a610952a81f815bad25b3a115ea3aeb
kyoongdev <9898junjun2@gmail.com>
2023-06-26 14:26:41 +0900

    feat(alarm): alarm events

--- src/event/fcm/event.ts
+++ src/event/fcm/event.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { JobCallback } from 'node-schedule';
 import { EventEmitter } from 'stream';
 
-import { CreateReservationUsageAlarm } from '@/interface/fcm.interface';
+import { CreateReservationUsageAlarm, CreateReviewRecommendAlarm } from '@/interface/fcm.interface';
 
 import { FCM_EVENT_NAME } from './constants';
 
@@ -13,5 +13,17 @@ export class FCMEvent {
 
   createReservationUsageAlarm(jobId: string, data: CreateReservationUsageAlarm, callback: JobCallback) {
     this.eventEmitter.emit(FCM_EVENT_NAME.CREATE_RESERVATION_USAGE_ALARM, jobId, data, callback);
+  }
+
+  createReviewRecommendAlarm(jobId: string, data: CreateReviewRecommendAlarm, callback: JobCallback) {
+    this.eventEmitter.emit(FCM_EVENT_NAME.CREATE_REVIEW_RECOMMEND_ALARM, jobId, data, callback);
+  }
+
+  createCouponDurationAlarm(jobId: string, data: CreateReviewRecommendAlarm, callback: JobCallback) {
+    this.eventEmitter.emit(FCM_EVENT_NAME.CREATE_COUPON_DURATION_ALARM, jobId, data, callback);
+  }
+
+  createQnAAnswerAlarm(jobId: string, data: CreateReviewRecommendAlarm, callback: JobCallback) {
+    this.eventEmitter.emit(FCM_EVENT_NAME.CREATE_QNA_ANSWER_ALARM, jobId, data, callback);
   }
 }
