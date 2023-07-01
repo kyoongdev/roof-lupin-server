c03e669650a39c4509ad74c68cfe8aa12a261b30
kyoongdev <9898junjun2@gmail.com>
2023-07-01 14:12:15 +0900

    feat(dto): update exhibition dto

--- src/modules/exhibition/dto/index.ts
+++ src/modules/exhibition/dto/index.ts
@@ -1,3 +1,4 @@
 export * from './exhibition.dto';
 export * from './exhibition-detail.dto';
 export * from './create-exhibition.dto';
+export * from './update-exhibition.dto';
---
+++ src/modules/exhibition/dto/update-exhibition.dto.ts
@@ -0,0 +1,56 @@
+import { Property } from 'wemacu-nestjs';
+
+export interface UpdateExhibitionDTOProps {
+  title?: string;
+  thumbnail?: string;
+  description?: string;
+  content?: string;
+  startAt?: Date;
+  endAt?: Date;
+  images?: string[];
+  spaceIds?: string[];
+  couponIds?: string[];
+}
+
+export class UpdateExhibitionDTO {
+  @Property({ apiProperty: { type: 'string', nullable: true, description: '기획전 제목' } })
+  title?: string;
+
+  @Property({ apiProperty: { type: 'string', nullable: true, description: '기획전 썸네일' } })
+  thumbnail?: string;
+
+  @Property({ apiProperty: { type: 'string', nullable: true, description: '기획전 부가 설명' } })
+  description?: string;
+
+  @Property({ apiProperty: { type: 'string', nullable: true, description: '기획전 내용' } })
+  content?: string;
+
+  @Property({ apiProperty: { type: 'string', nullable: true, description: '기획전 시작날짜' } })
+  startAt?: Date;
+
+  @Property({ apiProperty: { type: 'string', nullable: true, description: '기획전 종료날짜' } })
+  endAt?: Date;
+
+  @Property({ apiProperty: { type: 'string', nullable: true, description: '기획전 이미지 url들' } })
+  images?: string[];
+
+  @Property({ apiProperty: { type: 'string', nullable: true, description: '기획전 연관 공간 id들' } })
+  spaceIds?: string[];
+
+  @Property({ apiProperty: { type: 'string', nullable: true, description: '기획전 쿠폰 id들' } })
+  couponIds?: string[];
+
+  constructor(props?: UpdateExhibitionDTOProps) {
+    if (props) {
+      this.title = props.title;
+      this.thumbnail = props.thumbnail;
+      this.description = props.description;
+      this.content = props.content;
+      this.startAt = props.startAt;
+      this.endAt = props.endAt;
+      this.images = props.images;
+      this.spaceIds = props.spaceIds;
+      this.couponIds = props.couponIds;
+    }
+  }
+}
