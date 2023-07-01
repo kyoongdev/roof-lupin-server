8e28cd548a8f744d4af3902c5aca2023b6f1419f
kyoongdev <9898junjun2@gmail.com>
2023-07-01 15:02:42 +0900

    feat(exhibition): exhibition user apis

--- src/modules/exhibition/exhibition.controller.ts
+++ src/modules/exhibition/exhibition.controller.ts
@@ -1,8 +1,47 @@
-import { ApiController } from '@/utils';
+import { Get, Param } from '@nestjs/common';
 
+import { Auth, Paging, PagingDTO, RequestApi, ResponseApi } from 'wemacu-nestjs';
+
+import { ApiController } from '@/utils';
+import { JwtNullableAuthGuard } from '@/utils/guards';
+
+import { ExhibitionDetailDTO, ExhibitionDTO } from './dto';
 import { ExhibitionService } from './exhibition.service';
 
 @ApiController('exhibitions', '기획전')
 export class ExhibitionController {
   constructor(private readonly exhibitionService: ExhibitionService) {}
+
+  @Get(':exhibitionId/detail')
+  @Auth([JwtNullableAuthGuard])
+  @RequestApi({
+    summary: {
+      description: '기획전 상세 조회',
+      summary: '기획전 상세 조회',
+    },
+  })
+  @ResponseApi({
+    type: ExhibitionDetailDTO,
+  })
+  async getExhibition(@Param('exhibitionId') id: string) {
+    return this.exhibitionService.findExhibition(id);
+  }
+
+  @Get()
+  @RequestApi({
+    summary: {
+      description: '기획전 목록 조회',
+      summary: '기획전 목록 조회',
+    },
+    query: {
+      type: PagingDTO,
+    },
+  })
+  @ResponseApi({
+    type: ExhibitionDTO,
+    isPaging: true,
+  })
+  async getExhibitions(@Paging() paging: PagingDTO) {
+    return this.exhibitionService.findPagingExhibitions(paging);
+  }
 }
