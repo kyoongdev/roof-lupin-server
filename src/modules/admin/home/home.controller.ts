1cf7cf941c77f240f8cec279f11fa124b53694ba
kyoongdev <9898junjun2@gmail.com>
2023-07-13 18:54:22 +0900

    feat(home): admin home api

--- src/modules/admin/home/home.controller.ts
+++ src/modules/admin/home/home.controller.ts
@@ -1,8 +1,83 @@
-import { ApiController } from '@/utils';
+import { Body, Delete, Get, Param, Patch, Post, UseInterceptors } from '@nestjs/common';
+
+import { RequestApi, ResponseApi } from 'wemacu-nestjs';
+
+import { EmptyResponseDTO, ResponseWithIdDTO } from '@/common';
+import { CreateHomeContentsDTO, UpdateHomeContentsDTO } from '@/modules/home/dto';
+import { ApiController, ResponseWithIdInterceptor } from '@/utils';
+
+import { AdminHomeContentDTO } from '../dto/home';
 
 import { AdminHomeService } from './home.service';
 
 @ApiController('home', '[관리자] 홈 화면 관리')
 export class AdminHomeController {
   constructor(private readonly homeService: AdminHomeService) {}
+
+  @Get()
+  @RequestApi({
+    summary: {
+      description: '홈 화면 컨텐츠 불러오기',
+      summary: '홈 화면 컨텐츠 불러오기',
+    },
+  })
+  @ResponseApi({
+    type: AdminHomeContentDTO,
+    isArray: true,
+  })
+  async getHomeContents() {
+    return await this.homeService.findHomeContents();
+  }
+
+  @Post()
+  @UseInterceptors(ResponseWithIdInterceptor)
+  @RequestApi({
+    summary: {
+      description: '홈 화면 컨텐츠 생성하기',
+      summary: '홈 화면 컨텐츠 생성하기',
+    },
+  })
+  @ResponseApi(
+    {
+      type: ResponseWithIdDTO,
+    },
+    201
+  )
+  async createHomeContent(@Body() body: CreateHomeContentsDTO) {
+    return await this.homeService.createHomeContent(body);
+  }
+
+  @Patch(':homeContentId')
+  @RequestApi({
+    summary: {
+      description: '홈 화면 컨텐츠 수정하기',
+      summary: '홈 화면 컨텐츠 수정하기',
+    },
+  })
+  @ResponseApi(
+    {
+      type: EmptyResponseDTO,
+    },
+    204
+  )
+  async updateHomeContent(@Param('homeContentId') id: string, @Body() body: UpdateHomeContentsDTO) {
+    await this.homeService.updateHomeContent(id, body);
+  }
+
+  @Delete(':homeContentId')
+  @RequestApi({
+    summary: {
+      description: '홈 화면 컨텐츠 삭제하기',
+      summary: '홈 화면 컨텐츠 삭제하기',
+    },
+  })
+  @ResponseApi(
+    {
+      type: EmptyResponseDTO,
+    },
+    204
+  )
+  async deleteHomeContent(@Param('homeContentId') id: string) {
+    await this.homeService.deleteHomeContent(id);
+  }
 }
--- src/modules/admin/home/home.service.ts
+++ src/modules/admin/home/home.service.ts
@@ -7,6 +7,9 @@ import { CreateHomeContentsDTO, UpdateHomeContentsDTO } from '@/modules/home/dto
 import { HOME_AT_LEAST_ONE_TARGET, HOME_CONTENTS_NOT_FOUND, HOME_ERROR_CODE } from '@/modules/home/exception/errorCode';
 import { HomeException } from '@/modules/home/exception/home.exception';
 import { RankingRepository } from '@/modules/ranking/ranking.repository';
+import { SpaceDTO } from '@/modules/space/dto';
+
+import { AdminHomeContentDTO } from '../dto/home';
 
 @Injectable()
 export class AdminHomeService {
@@ -30,25 +33,75 @@ export class AdminHomeService {
     return content;
   }
 
+  async findHomeContents() {
+    const contents = await this.database.homeContents.findMany({
+      include: {
+        contentsCategories: {
+          include: {
+            spaces: {
+              include: {
+                space: {
+                  include: {
+                    location: true,
+                    reviews: true,
+                    publicTransportations: true,
+                    userInterests: true,
+                    rentalType: true,
+                  },
+                },
+              },
+              orderBy: {
+                orderNo: 'asc',
+              },
+            },
+          },
+        },
+        exhibitions: true,
+        rankings: {
+          include: {
+            spaces: {
+              include: {
+                space: {
+                  include: {
+                    location: true,
+                    reviews: true,
+                    publicTransportations: true,
+                    userInterests: true,
+                    rentalType: true,
+                  },
+                },
+              },
+              orderBy: {
+                orderNo: 'asc',
+              },
+            },
+          },
+        },
+      },
+      orderBy: {
+        orderNo: 'asc',
+      },
+    });
+    return contents.map(
+      (content) =>
+        new AdminHomeContentDTO({
+          ...content,
+          contentsCategories: content.contentsCategories.map((content) => ({
+            ...content,
+            spaces: content.spaces.map((space) => SpaceDTO.generateSpaceDTO(space.space)),
+          })),
+          rankings: content.rankings.map((ranking) => ({
+            ...ranking,
+            spaces: ranking.spaces.map((space) => SpaceDTO.generateSpaceDTO(space.space)),
+          })),
+        })
+    );
+  }
+
   async createHomeContent(data: CreateHomeContentsDTO) {
-    const targets = [data.contentCategoryId, data.exhibitionId, data.rankingId];
+    this.validateMutatingHomeContent(data);
 
-    if (targets.filter(Boolean).length !== 1) {
-      throw new HomeException(HOME_ERROR_CODE.BAD_REQUEST(HOME_AT_LEAST_ONE_TARGET));
-    }
-
-    if (data.contentCategoryId) {
-      await this.categoryRepository.findContentCategory(data.contentCategoryId);
-    }
-    if (data.exhibitionId) {
-      await this.exhibitionRepository.findExhibition(data.exhibitionId);
-    }
-
-    if (data.rankingId) {
-      await this.rankingRepository.findRanking(data.rankingId);
-    }
-
-    await this.database.homeContents.create({
+    const content = await this.database.homeContents.create({
       data: {
         orderNo: data.orderNo,
         ...(data.contentCategoryId && {
@@ -74,27 +127,12 @@ export class AdminHomeService {
         }),
       },
     });
+    return content;
   }
 
   async updateHomeContent(id: string, data: UpdateHomeContentsDTO) {
     await this.findHomeContent(id);
-
-    const targets = [data.contentCategoryId, data.exhibitionId, data.rankingId];
-
-    if (targets.filter(Boolean).length !== 1) {
-      throw new HomeException(HOME_ERROR_CODE.BAD_REQUEST(HOME_AT_LEAST_ONE_TARGET));
-    }
-
-    if (data.contentCategoryId) {
-      await this.categoryRepository.findContentCategory(data.contentCategoryId);
-    }
-    if (data.exhibitionId) {
-      await this.exhibitionRepository.findExhibition(data.exhibitionId);
-    }
-
-    if (data.rankingId) {
-      await this.rankingRepository.findRanking(data.rankingId);
-    }
+    this.validateMutatingHomeContent(data);
 
     await this.database.homeContents.update({
       where: {
@@ -134,5 +172,24 @@ export class AdminHomeService {
         id,
       },
     });
+  }
+
+  async validateMutatingHomeContent(data: CreateHomeContentsDTO | UpdateHomeContentsDTO) {
+    const targets = [data.contentCategoryId, data.exhibitionId, data.rankingId];
+
+    if (targets.filter(Boolean).length !== 1) {
+      throw new HomeException(HOME_ERROR_CODE.BAD_REQUEST(HOME_AT_LEAST_ONE_TARGET));
+    }
+
+    if (data.contentCategoryId) {
+      await this.categoryRepository.findContentCategory(data.contentCategoryId);
+    }
+    if (data.exhibitionId) {
+      await this.exhibitionRepository.findExhibition(data.exhibitionId);
+    }
+
+    if (data.rankingId) {
+      await this.rankingRepository.findRanking(data.rankingId);
+    }
   }
 }
