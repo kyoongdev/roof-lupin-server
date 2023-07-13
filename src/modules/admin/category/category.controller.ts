da83f5f4a436c747560d5d54d50663b22f90c386
kyoongdev <9898junjun2@gmail.com>
2023-07-13 10:51:41 +0900

    feat(category): find categories query

--- src/modules/admin/category/category.controller.ts
+++ src/modules/admin/category/category.controller.ts
@@ -1,8 +1,42 @@
+import { Get, Param } from '@nestjs/common';
+
+import { Auth, Paging, PagingDTO, RequestApi, ResponseApi } from 'wemacu-nestjs';
+
+import { CategoryDTO } from '@/modules/category/dto';
 import { ApiController } from '@/utils';
+import { JwtAuthGuard } from '@/utils/guards';
+import { RoleGuard } from '@/utils/guards/role.guard';
 
 import { AdminCategoryService } from './category.service';
 
+@Auth([JwtAuthGuard, RoleGuard('ADMIN')])
 @ApiController('/categories', '[관리자] 카테고리 관리')
 export class AdminCategoryController {
   constructor(private readonly categoryService: AdminCategoryService) {}
+
+  @Get(':categoryId/detail')
+  @RequestApi({
+    summary: {
+      description: '카테고리 불러오기',
+      summary: '카테고리 자세히 불러오기',
+    },
+  })
+  @ResponseApi({
+    type: CategoryDTO,
+  })
+  async getCategory(@Param('categoryId') id: string) {
+    return await this.categoryService.findCategory(id);
+  }
+
+  @Get('')
+  @RequestApi({
+    summary: {
+      description: '카테고리 목록 불러오기',
+      summary: '카테고리 목록 불러오기',
+    },
+  })
+  @ResponseApi({ type: CategoryDTO, isPaging: true })
+  async getCategories(@Paging() paging: PagingDTO) {
+    return await this.categoryService.findPagingCategories(paging);
+  }
 }
---
+++ src/modules/category/dto/query/find-categories.query.ts
@@ -0,0 +1,33 @@
+import { Prisma } from '@prisma/client';
+import { PagingDTO, Property, ToBoolean } from 'wemacu-nestjs';
+
+export class FindCategoriesQuery extends PagingDTO {
+  @ToBoolean()
+  @Property({ apiProperty: { type: 'boolean', nullable: true, description: '홈 화면 유무' } })
+  isHome?: boolean;
+
+  @ToBoolean()
+  @Property({ apiProperty: { type: 'boolean', nullable: true, description: '추천 유무' } })
+  isRecommend?: boolean;
+
+  @Property({ apiProperty: { type: 'string', nullable: true, description: '이름' } })
+  name?: string;
+
+  generateQuery(): Prisma.CategoryFindManyArgs {
+    return {
+      where: {
+        ...(typeof this.isHome === 'boolean' && {
+          isHome: this.isHome,
+        }),
+        ...(typeof this.isRecommend === 'boolean' && {
+          isRecommend: this.isRecommend,
+        }),
+        ...(this.name && {
+          name: {
+            contains: this.name,
+          },
+        }),
+      },
+    };
+  }
+}
