f0bab7087ec42d2f34beb6cb23576a25155c7f41
kyoongdev <9898junjun2@gmail.com>
2023-07-19 14:37:53 +0900

    feat(admin): user logined at

--- src/modules/admin/dto/user/user.dto.ts
+++ src/modules/admin/dto/user/user.dto.ts
@@ -4,13 +4,18 @@ import { CommonUserDTO, CommonUserProps } from '@/modules/user/dto';
 
 export interface AdminUserDTOProps extends CommonUserProps {
   isBlocked: boolean;
+  loginedAt?: Date;
 }
 export class AdminUserDTO extends CommonUserDTO {
   @Property({ apiProperty: { type: 'boolean', description: '차단 여부' } })
   isBlocked: boolean;
 
+  @Property({ apiProperty: { type: 'boolean', nullable: true, description: '차단 여부' } })
+  loginedAt?: Date;
+
   constructor(props: AdminUserDTOProps) {
     super(props);
     this.isBlocked = props.isBlocked;
+    this.loginedAt = props.loginedAt;
   }
 }
