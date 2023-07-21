83fe395e96e237e94cad2bb6cdf8ffd4052c7b24
kyoongdev <9898junjun2@gmail.com>
2023-07-21 16:26:04 +0900

    feat(aws): code deploy scripts

---
+++ scripts/start_server.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo chmod -R 777 /home/ec2-user/build
+
+cd /home/ec2-user/build
+echo "cd to build File"
+source /home/ec2-user/.bash_profile
+
+npm -v
+node -v
+
+npm install -g yarn 
+
+yarn -v
+
+npm install -g pm2
+
+cp /home/ec2-user/build/env/.env.dev /home/ec2-user/build/.env
+
+pm2 stop all
+
+yarn config:dev
+yarn build
+
+pm2 start dist/ecosystem.json
