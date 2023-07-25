13751edd4de1cd432183292b9e69e2caaeb79e20
kyoongdev <9898junjun2@gmail.com>
2023-07-25 22:04:28 +0900

    feat(space): space dto orderNo

--- src/modules/admin/dto/query/space/find-spaces.query.ts
+++ src/modules/admin/dto/query/space/find-spaces.query.ts
@@ -1,9 +1,17 @@
 import { Prisma } from '@prisma/client';
-import { PagingDTO, Property } from 'wemacu-nestjs';
+import { PagingDTO, Property, ToBoolean } from 'wemacu-nestjs';
 
 export class AdminFindSpacesQuery extends PagingDTO {
   @Property({ apiProperty: { type: 'string', nullable: true, description: '공간 이름' } })
   title?: string;
+
+  @ToBoolean()
+  @Property({ apiProperty: { type: 'boolean', nullable: true, description: '공간 승인 여부' } })
+  isApproved?: boolean;
+
+  @ToBoolean()
+  @Property({ apiProperty: { type: 'boolean', nullable: true, description: '공간 노출 여부' } })
+  isPublic?: boolean;
 
   generateQuery(): Prisma.SpaceFindManyArgs {
     return {
