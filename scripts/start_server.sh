3e0b3b983b787a9b6ed2732f7297067e49683c57
kyoongdev <9898junjun2@gmail.com>
2023-07-25 13:11:27 +0900

    chore(codebuild): build test

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -6,19 +6,8 @@ cd /home/ec2-user/build
 echo "cd to build File"
 source /home/ec2-user/.bash_profile
 
-npm -v
-node -v
-
-npm install -g yarn 
-
-yarn -v
-yarn install
-npm install -g pm2
-
 pm2 kill
 
-yarn config:dev
-yarn build
 yarn prisma db push --accept-data-loss	
 yarn seed
 
