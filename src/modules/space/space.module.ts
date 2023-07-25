e7051f7de1a2680425b4016df152790ae411eabb
kyoongdev <9898junjun2@gmail.com>
2023-07-25 14:24:53 +0900

    feat(module): rentaltype

--- src/modules/space/space.module.ts
+++ src/modules/space/space.module.ts
@@ -1,15 +1,26 @@
 import { Module } from '@nestjs/common';
 
 import { HolidayService } from '../holiday/holiday.service';
+import { BlockedTimeRepository } from '../host/blocked-time/blocked-time.repository';
 import { SearchRepository } from '../search/search.repository';
 
+import { RentalTypeController } from './rental-type/rental-type.controller';
 import { RentalTypeRepository } from './rental-type/rental-type.repository';
+import { RentalTypeService } from './rental-type/rental-type.service';
 import { SpaceController } from './space.controller';
 import { SpaceRepository } from './space.repository';
 import { SpaceService } from './space.service';
 
 @Module({
-  providers: [SpaceService, SpaceRepository, RentalTypeRepository, HolidayService, SearchRepository],
-  controllers: [SpaceController],
+  providers: [
+    SpaceService,
+    SpaceRepository,
+    RentalTypeRepository,
+    HolidayService,
+    SearchRepository,
+    RentalTypeService,
+    BlockedTimeRepository,
+  ],
+  controllers: [SpaceController, RentalTypeController],
 })
 export class SpaceModule {}
