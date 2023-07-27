e04077f7ce7ffb472ef0a5b6e54340303373a219
kyoongdev <9898junjun2@gmail.com>
2023-07-27 16:03:30 +0900

    feat(space): floor to number

---
+++ prisma/migrations/20230727070222_floor/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `defaultDueDateStart` on the `Coupon` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+  - You are about to alter the column `startAt` on the `Exhibition` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+  - You are about to alter the column `endAt` on the `Exhibition` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+  - You are about to alter the column `floor` on the `SpaceSize` table. The data in that column could be lost. The data in that column will be cast from `VarChar(10)` to `TinyInt`.
+  - You are about to alter the column `usageDateStartAt` on the `UserCoupon` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+  - You are about to alter the column `usageDateEndAt` on the `UserCoupon` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+
+*/
+-- AlterTable
+ALTER TABLE `Coupon` MODIFY `defaultDueDateStart` DATETIME NULL;
+
+-- AlterTable
+ALTER TABLE `Exhibition` MODIFY `startAt` DATETIME NOT NULL,
+    MODIFY `endAt` DATETIME NOT NULL;
+
+-- AlterTable
+ALTER TABLE `SpaceSize` MODIFY `floor` TINYINT NOT NULL;
+
+-- AlterTable
+ALTER TABLE `UserCoupon` MODIFY `usageDateStartAt` DATETIME NOT NULL,
+    MODIFY `usageDateEndAt` DATETIME NOT NULL;
