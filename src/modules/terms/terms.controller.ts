23e0089ca268b8d8f13e9c911be58174258dd10d
kyoongdev <9898junjun2@gmail.com>
2023-08-02 16:49:21 +0900

    feat(terms): terms enum for swagger

--- src/modules/terms/terms.controller.ts
+++ src/modules/terms/terms.controller.ts
@@ -2,6 +2,7 @@ import { Get, Param } from '@nestjs/common';
 
 import { RequestApi, ResponseApi } from 'cumuco-nestjs';
 
+import { GUEST_TERMS, HOST_TERMS } from '@/common/constants/terms';
 import { ApiController } from '@/utils';
 
 import { TermDTO } from './dto';
@@ -31,6 +32,11 @@ export class TermsController {
     summary: {
       description: '약관 단건 불러오기',
       summary: '약관 단건 불러오기',
+    },
+    params: {
+      type: 'string',
+      enum: [...Object.values(GUEST_TERMS), ...Object.values(HOST_TERMS)],
+      name: '약관 이름',
     },
   })
   @ResponseApi({
