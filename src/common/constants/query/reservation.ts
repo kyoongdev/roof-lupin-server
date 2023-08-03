ddfc377ba582ff33271ddc5f71ffadb030ed04a5
kyoongdev <9898junjun2@gmail.com>
2023-08-03 10:40:17 +0900

    feat(reservation): additional service count

--- src/common/constants/query/reservation.ts
+++ src/common/constants/query/reservation.ts
@@ -1,5 +1,7 @@
 import { Prisma } from '@prisma/client';
 
+import { SpaceDTO } from '@/modules/space/dto';
+
 export const reservationInclude: Prisma.ReservationInclude = {
   user: true,
   rentalTypes: {
@@ -8,13 +10,7 @@ export const reservationInclude: Prisma.ReservationInclude = {
         include: {
           timeCostInfo: true,
           space: {
-            include: {
-              reviews: true,
-              location: true,
-              publicTransportations: true,
-              userInterests: true,
-              rentalType: true,
-            },
+            include: SpaceDTO.getSpacesIncludeOption(),
           },
           additionalServices: true,
         },
