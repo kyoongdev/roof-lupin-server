27aa1822307dbf3cd16b7bfdc4572da81a5adf44
kyoongdev <9898junjun2@gmail.com>
2023-08-04 23:30:06 +0900

    chore(deploy): before install

--- src/utils/validation/terms.validation.ts
+++ src/utils/validation/terms.validation.ts
@@ -6,7 +6,6 @@ import { Transform } from 'class-transformer';
 import { GUEST_TERMS, GUEST_TERMS_KOR, HOST_TERMS, HOST_TERMS_KOR } from '@/common/constants/terms';
 
 export const termEngToKor = (name: string) => {
-  console.log(name);
   const term = GUEST_TERMS_KOR[name] ? GUEST_TERMS_KOR[name] : HOST_TERMS_KOR[name] ? HOST_TERMS_KOR[name] : null;
 
   return term;
