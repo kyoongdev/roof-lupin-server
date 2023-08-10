d312fa6161a727664a51c82c1a3701e48b40c0bb
kyoongdev <9898junjun2@gmail.com>
2023-08-10 14:17:44 +0900

    feat(location-filter): module

---
+++ src/modules/location-filter/location-filter.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+
+import { LocationFilterRepository } from './location-filter.repository';
+
+@Injectable()
+export class LocationFilterService {
+  constructor(private readonly locationFilterRepository: LocationFilterRepository) {}
+
+  async findLocationFilter() {
+    const locationFilters = await this.locationFilterRepository.findLocationFilters({
+      where: {
+        group: {
+          name: '서울',
+        },
+      },
+    });
+
+    return locationFilters.at(-1);
+  }
+}
