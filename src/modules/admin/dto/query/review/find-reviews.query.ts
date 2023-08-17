24846e8c97100af66e4476f53542dc8a2311aeb2
kyoongdev <9898junjun2@gmail.com>
2023-08-17 14:50:38 +0900

    feat(review): admin find reviews query

--- src/modules/admin/dto/query/review/find-reviews.query.ts
+++ src/modules/admin/dto/query/review/find-reviews.query.ts
@@ -5,11 +5,17 @@ export class AdminFindReviewsQuery extends PagingDTO {
   @Property({ apiProperty: { type: 'string', nullable: true, description: '유저 id' } })
   userId?: string;
 
+  @Property({ apiProperty: { type: 'string', nullable: true, description: '공간 id' } })
+  spaceId?: string;
+
   generateQuery(): Prisma.SpaceReviewFindManyArgs {
     return {
       where: {
         ...(this.userId && {
           userId: this.userId,
+        }),
+        ...(this.spaceId && {
+          spaceId: this.spaceId,
         }),
       },
     };
