d49d0f875c0fad62c87d9fbb41958240da0feb66
kyoongdev <9898junjun2@gmail.com>
2023-08-20 11:50:59 +0900

    feat(admin): mutate host api:

--- src/modules/admin/host/host.service.ts
+++ src/modules/admin/host/host.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 
 import type { Prisma } from '@prisma/client';
 import { PaginationDTO, PagingDTO } from 'cumuco-nestjs';
 
-import { HostDTO } from '@/modules/host/dto';
+import { BlockHostDTO, CreateHostDTO, HostDTO, UpdateHostDTO } from '@/modules/host/dto';
+import { HOST_ALREADY_EXIST } from '@/modules/host/exception/errorCode';
 import { HostRepository } from '@/modules/host/host.repository';
 
 @Injectable()
@@ -26,5 +27,40 @@ export class AdminHostService {
 
   async findHost(id: string) {
     return await this.hostRepository.findHostDetail(id);
+  }
+
+  async createHost(data: CreateHostDTO) {
+    const isExist = await this.hostRepository.checkHostByEmail(data.email);
+
+    if (isExist) {
+      throw new ConflictException(HOST_ALREADY_EXIST);
+    }
+
+    return await this.hostRepository.createHost(data);
+  }
+
+  async updateHost(id: string, data: UpdateHostDTO) {
+    await this.findHost(id);
+    await this.hostRepository.updateHost(id, data);
+  }
+
+  async blockHost(id: string, data: BlockHostDTO) {
+    await this.findHost(id);
+    await this.hostRepository.blockHost(id, data);
+  }
+
+  async unBlockHost(id: string) {
+    await this.findHost(id);
+    await this.hostRepository.unBlockHost(id);
+  }
+
+  async deleteHost(id: string) {
+    await this.findHost(id);
+    await this.hostRepository.deleteHost(id);
+  }
+
+  async hardDeleteHost(id: string) {
+    await this.findHost(id);
+    await this.hostRepository.hardDeleteHost(id);
   }
 }
