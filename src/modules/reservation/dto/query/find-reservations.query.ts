0b2c699a325953f9c70166d4ce149ac43cc21163
kyoongdev <9898junjun2@gmail.com>
2023-08-24 18:05:00 +0900

    feat(reservation): query with isCanceled

--- src/modules/reservation/dto/query/find-reservations.query.ts
+++ src/modules/reservation/dto/query/find-reservations.query.ts
@@ -6,11 +6,18 @@ export class FindReservationQuery extends PagingDTO {
   @Property({ apiProperty: { type: 'boolean', description: '승인 여부' } })
   isApproved?: boolean;
 
+  @ToBoolean()
+  @Property({ apiProperty: { type: 'boolean', description: '취소 여부' } })
+  isCanceled?: boolean;
+
   generateQuery(): Prisma.ReservationFindManyArgs {
     return {
       where: {
         ...(typeof this.isApproved === 'boolean' && {
           isApproved: this.isApproved,
+        }),
+        ...(typeof this.isApproved === 'boolean' && {
+          isCanceled: this.isCanceled,
         }),
       },
     };
--- src/modules/review/review.service.ts
+++ src/modules/review/review.service.ts
@@ -60,6 +60,7 @@ export class ReviewService {
         deletedAt: null,
       }
     );
+
     return review;
   }
 
