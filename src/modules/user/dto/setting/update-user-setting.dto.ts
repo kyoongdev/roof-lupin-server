2fea7db17a547916f9db116c0b2f56f777ad1d11
kyoongdev <9898junjun2@gmail.com>
2023-09-05 19:12:50 +0900

    feat(dto): user setting

---
+++ src/modules/user/dto/setting/update-user-setting.dto.ts
@@ -0,0 +1,39 @@
+import { Property } from 'cumuco-nestjs';
+
+export interface UpdateUserSettingDTOProps {
+  isAdult?: boolean;
+  isAlarmAccepted?: boolean;
+  isLocationInfoAccepted?: boolean;
+  isEmailAccepted?: boolean;
+  isKakaoTalkAccepted?: boolean;
+  isPushAccepted?: boolean;
+}
+
+export class UpdateUserSettingDTO {
+  @Property({ apiProperty: { type: 'boolean', nullable: true, description: '성인 인증 여부' } })
+  isAdult?: boolean;
+
+  @Property({ apiProperty: { type: 'boolean', nullable: true, description: '알림 수신 동의 여부' } })
+  isAlarmAccepted?: boolean;
+
+  @Property({ apiProperty: { type: 'boolean', nullable: true, description: '위치 정보 수집 동의 여부' } })
+  isLocationInfoAccepted?: boolean;
+
+  @Property({ apiProperty: { type: 'boolean', nullable: true, description: '이메일 수신 동의 여부' } })
+  isEmailAccepted?: boolean;
+
+  @Property({ apiProperty: { type: 'boolean', nullable: true, description: '카카오톡 알림 수신 동의 여부' } })
+  isKakaoTalkAccepted?: boolean;
+
+  @Property({ apiProperty: { type: 'boolean', nullable: true, description: '푸시 알림 수신 동의 여부' } })
+  isPushAccepted?: boolean;
+
+  constructor(props: UpdateUserSettingDTOProps) {
+    this.isAdult = props.isAdult;
+    this.isAlarmAccepted = props.isAlarmAccepted;
+    this.isLocationInfoAccepted = props.isLocationInfoAccepted;
+    this.isEmailAccepted = props.isEmailAccepted;
+    this.isKakaoTalkAccepted = props.isKakaoTalkAccepted;
+    this.isPushAccepted = props.isPushAccepted;
+  }
+}
--- src/modules/user/user.repository.ts
+++ src/modules/user/user.repository.ts
@@ -165,6 +165,7 @@ export class UserRepository {
             socialType,
           },
         },
+        setting: {},
       },
     });
 
