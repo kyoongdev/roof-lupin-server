d061d4ae673eb68a321c1f06ffbce96b17fabea8
kyoongdev <9898junjun2@gmail.com>
2023-09-14 15:11:01 +0900

    feat(module): location module injection

--- src/modules/host/location/location.module.ts
+++ src/modules/host/location/location.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+import { SocialLocationModule, SocialLocationService } from 'cumuco-nestjs';
 
 import { LocationService } from '@/modules/location/location.service';
 
 import { HostLocationController } from './location.controller';
-
+const config = new ConfigService();
 @Module({
+  imports: [
+    SocialLocationModule.forRoot({
+      kakaoRestKey: config.get('KAKAO_REST_KEY'),
+      naver: {
+        clientId: config.get('NAVER_CONSOLE_CLIENT_ID'),
+        clientSecret: config.get('NAVER_CONSOLE_CLIENT_SECRET'),
+      },
+    }),
+  ],
   providers: [LocationService],
   controllers: [HostLocationController],
 })
