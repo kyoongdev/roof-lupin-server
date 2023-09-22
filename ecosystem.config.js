7dee8e6652cbd5f9938eda8fa6a3702c4fca8138
kyoongdev <9898junjun2@gmail.com>
2023-09-22 17:16:47 +0900

    chore(build): start with build

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,9 @@ module.exports = {
   apps: [
     {
       name: 'api',
-      script: 'src/main.ts',
+      script: 'dist/main.js',
       watch: true,
       exec_mode: 'cluster',
-      node_args: '-r ts-node/register -r tsconfig-paths/register',
-      interpreter: 'node_modules/.bin/ts-node',
       instances: 0,
     },
   ],
