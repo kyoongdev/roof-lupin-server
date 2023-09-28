e74ba653372fdbdceefc8aec466d2bcc73cd4623
kyoongdev <9898junjun2@gmail.com>
2023-09-28 14:04:28 +0900

    feat(admin): reservations query

--- src/modules/admin/dto/query/reservation/find-reservations.query.ts
+++ src/modules/admin/dto/query/reservation/find-reservations.query.ts
@@ -5,11 +5,43 @@ export class AdminFindReservationsQuery extends PagingDTO {
   @Property({ apiProperty: { type: 'string', nullable: true, description: '유저 id' } })
   userId?: string;
 
+  @Property({ apiProperty: { type: 'string', nullable: true, description: '유저 이름' } })
+  userName?: string;
+
+  @Property({ apiProperty: { type: 'string', nullable: true, description: '예약 코드' } })
+  code?: string;
+
+  @Property({ apiProperty: { type: 'number', nullable: true, description: '예약 연도' } })
+  year?: number;
+
+  @Property({ apiProperty: { type: 'number', nullable: true, description: '예약 월' } })
+  month?: number;
+
+  @Property({ apiProperty: { type: 'number', nullable: true, description: '예약 일' } })
+  day?: number;
+
   generateQuery(): Prisma.ReservationFindManyArgs {
     return {
       where: {
         ...(this.userId && {
           userId: this.userId,
+        }),
+        ...(this.userName && {
+          user: {
+            name: this.userName,
+          },
+        }),
+        ...(this.code && {
+          code: this.code,
+        }),
+        ...(this.year && {
+          year: this.year,
+        }),
+        ...(this.month && {
+          month: this.month,
+        }),
+        ...(this.day && {
+          day: this.day,
         }),
       },
     };
