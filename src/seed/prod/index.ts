7edd6964693c6f875081f24b0aea4b92ea2f2d21
kyoongdev <9898junjun2@gmail.com>
2023-10-11 17:31:29 +0900

    chore(prod): seed

--- src/seed/prod/index.ts
+++ src/seed/prod/index.ts
@@ -15,6 +15,24 @@ import { seedSpace } from './space';
 export const seedDatabase = async (database: PrismaClient) => {
   const admin = await database.admin.findFirst({});
 
+  // if (admin) return;
+  await database.space.deleteMany({});
+  await database.host.deleteMany({});
+  await database.user.deleteMany({});
+  await database.admin.deleteMany({});
+  await database.location.deleteMany({});
+  await database.category.deleteMany({});
+  await database.announcement.deleteMany({});
+  await database.coupon.deleteMany({});
+  await database.homeContents.deleteMany({});
+  await database.curation.deleteMany({});
+  await database.ranking.deleteMany({});
+  await database.fAQ.deleteMany({});
+  await database.spaceHoliday.deleteMany({});
+  await database.exhibition.deleteMany({});
+  await database.locationFilterGroup.deleteMany({});
+  await database.icon.deleteMany({});
+  await database.serviceTitle.deleteMany({});
   await database.space.deleteMany({});
   await database.host.deleteMany({});
   await database.user.deleteMany({});
