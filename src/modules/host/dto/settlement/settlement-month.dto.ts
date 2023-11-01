8803ef3e7b5a6048824ced8b2cfa59a89dbc8f2a
kyoongdev <9898junjun2@gmail.com>
2023-11-01 13:39:12 +0900

    feat(settlement): month dto

--- src/modules/host/dto/settlement/settlement-month.dto.ts
+++ src/modules/host/dto/settlement/settlement-month.dto.ts
@@ -1,17 +1,23 @@
 import { Property } from 'cumuco-nestjs';
 
 export interface SettlementMonthDTOProps {
+  id: string;
   year: number;
   month: number;
 }
 
 export class SettlementMonthDTO {
+  @Property({ apiProperty: { type: 'string', description: 'id' } })
+  id: string;
+
   @Property({ apiProperty: { type: 'number', description: '정상 연도' } })
   year: number;
+
   @Property({ apiProperty: { type: 'number', description: '정상 월' } })
   month: number;
 
   constructor(props: SettlementMonthDTOProps) {
+    this.id = props.id;
     this.year = props.year;
     this.month = props.month;
   }
