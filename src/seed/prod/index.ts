db256165677cc5670792ecd518e6c0cab62e379f
kyoongdev <9898junjun2@gmail.com>
2023-11-16 22:17:46 +0900

    feat(db): re init

--- src/seed/prod/index.ts
+++ src/seed/prod/index.ts
@@ -3,6 +3,26 @@ import { PrismaClient } from '@prisma/client';
 import { EncryptProvider } from '../../common/encrypt';
 
 export const seedDatabase = async (database: PrismaClient) => {
+  await database.space.deleteMany({});
+  await database.host.deleteMany({});
+  await database.user.deleteMany({});
+  await database.admin.deleteMany({});
+  await database.location.deleteMany({});
+  await database.category.deleteMany({});
+  await database.announcement.deleteMany({});
+  await database.coupon.deleteMany({});
+  await database.homeContents.deleteMany({});
+  await database.curation.deleteMany({});
+  await database.ranking.deleteMany({});
+  await database.fAQ.deleteMany({});
+  await database.spaceHoliday.deleteMany({});
+  await database.exhibition.deleteMany({});
+  await database.frequentQuestion.deleteMany({});
+  await database.image.deleteMany({});
+  // await database.searchRecommend.deleteMany({});
+  // await database.locationFilterGroup.deleteMany({});
+  // await database.icon.deleteMany({});
+  // await database.serviceTitle.deleteMany({});
   const admin = await database.admin.findFirst({});
 
   if (admin) return;
